8be3c05c88181eda7745c04e7f540df10f11683b
yash2870 <94793349+yash2870@users.noreply.github.com>
2025-01-25 15:06:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import Education from './components/Education';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
-import Contact from './components/contact';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
