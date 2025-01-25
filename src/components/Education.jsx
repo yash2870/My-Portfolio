d2db0652a499af44a8df5ab31bf75d19201e7a9e
yash2870 <94793349+yash2870@users.noreply.github.com>
2025-01-25 14:53:30 +0530

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,24 +8,34 @@ const Education = () => {
     <section
       id="education"
       ref={ref}
-      className={`py-20 bg-gradient-to-b from-gray-900 to-black text-center text-white transition-transform duration-1000 ${
-        inView ? " opacity-100" : " opacity-0"
+      className={`py-20 bg-gradient-to-b from-gray-900 to-black text-center text-white transition-opacity duration-1000 ${
+        inView ? "opacity-100" : "opacity-0"
       }`}
     >
       <h2 className="text-3xl font-bold mb-8">Education</h2>
       <div className="space-y-8">
-        <div>
-          <h3 className="text-xl font-semibold">Bachelor of Technology</h3>
-          <p>MCKV Institute Of Engineering (affiliated to MAKAUT), 2021 - Present</p>
+        {/* Bachelor of Technology */}
+        <div className="p-6 bg-gray-800 rounded-lg shadow-lg ">
+          <h3 className="text-xl font-semibold mb-2">Bachelor of Technology</h3>
+          <p className="mb-2">
+            MCKV Institute Of Engineering (affiliated to MAKAUT), 2021 - Present
+          </p>
           <p>Computer Science and Engineering (Data Science)</p>
+          <p>Grade:86.3%</p>
         </div>
-        <div>
-          <h3 className="text-xl font-semibold">Higher Secondary (10+2)</h3>
+
+        {/* Higher Secondary (10+2) */}
+        <div className="p-6 bg-gray-800 rounded-lg shadow-lg ">
+          <h3 className="text-xl font-semibold mb-2">Higher Secondary (10+2)</h3>
           <p>Vivekananda Academy, 2021</p>
+          <p>Grade:81.17%</p>
         </div>
-        <div>
-          <h3 className="text-xl font-semibold">Secondary</h3>
+
+        {/* Secondary */}
+        <div className="p-6 bg-gray-800 rounded-lg shadow-lg">
+          <h3 className="text-xl font-semibold mb-2">Secondary</h3>
           <p>Gospel Home School, 2019</p>
+          <p>Grade:90.8%</p>
         </div>
       </div>
     </section>
